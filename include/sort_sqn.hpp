9580be42211d3c44e02432479884a8a25fd05a4e
DanielVT-ECE <danielmurray_@outlook.com>
2025-11-05 16:03:49 -0500

    fixing sort_sqn

--- include/sort_sqn.hpp
+++ include/sort_sqn.hpp
@@ -1,13 +1,23 @@
 #include <vector>
+#include <queue>
 std::vector<int> top_sqrtN_sorted(const std::vector<int>& A){
     using namespace std;
     int N = A.size();
-    int M = static_cast<int>(sqrt(N));
-    vector<int> result;
-    result.reserve(M);
+    int k = static_cast<int>(sqrt(N));
     
-    for(int i = N - M; i < N; ++i){
-        result.push_back(A[i]);
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    
+    for(int i = 0; i < N; ++i){
+        minHeap.push(A[i]);
+        if(minHeap.size() > k){
+            minHeap.pop();
+        }
+    }
+    
+    vector<int> result;
+    while(!minHeap.empty()){
+        result.push_back(minHeap.top());
+        minHeap.pop();
     }
     
     sort(result.begin(), result.end());
